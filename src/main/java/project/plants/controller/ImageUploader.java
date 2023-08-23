aee0e2551cfab6f952413ed2ac6d49c1ae35570e
jeong-sys <20211939@edu.hanbat.ac.kr>
2023-08-23 10:31:01 +0900

    edit upload code

--- src/main/java/project/plants/controller/ImageUploader.java
+++ src/main/java/project/plants/controller/ImageUploader.java
@@ -5,39 +5,35 @@ import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class ImageUploader{
-
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/plants";
-        String user = "root";
-        String password = "12345678";
-
-        // DB에 사진 업로드 후, 제대로 업로드 되었는지 확인해보기.
 
         File imageFile = new File("/Users/heoyujeong/r&e/plants/src/main/resources/static/img/output2.png"); // 사진 파일 경로
 
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             FileInputStream fis = new FileInputStream(imageFile);
-             PreparedStatement statement = connection.prepareStatement("INSERT INTO plants (ID, FILENAME, FILE) VALUES (?, ?, ?)")) {
-            // key가 없음
+        try {
+            // DB 정보
+            String url = "jdbc:mysql://localhost:3306/plants";
+            String user = "root";
+            String password = "12345678";
+
+            Connection con = DriverManager.getConnection(url, user, password); // DB 연결
+            FileInputStream fis = new FileInputStream(imageFile); // FileInputStream : 파일로부터 바이트 입력받아 바이트 단위로 출력
+
+            // SQL 구문 실행 Statement 클래스 기능 향, 매개변수(인자) 관련 작업에 특화
+            PreparedStatement statement = con.prepareStatement("INSERT INTO plants (ID, FILENAME, FILE) VALUES (?, ?, ?)");
 
             statement.setInt(1,1); // set을 이용해서 "?" 자리에 값을 집어넣음, 인덱스 해당 숫자 : 물음표의 순번
             statement.setString(2, "X-ray");
             statement.setBinaryStream(3, fis, (int)imageFile.length()); // Stream 형의 파일 업로드
             statement.executeUpdate();
 
-//            byte[] imageData = new byte[(int) imageFile.length()];
-//            fis.read(imageData);
-//
-//            statement.setBytes(1, imageData);
-//            statement.executeUpdate();
-
-            System.out.println("Image uploaded successfully!");
+            System.out.println("이미지 업로드 성공");
             statement.close();
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("업로드 실패");
         }
     }
-}
+    }
