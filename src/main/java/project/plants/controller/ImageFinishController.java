3bea5b6b7d10ada9f65cbf90b96e8bf943242a01
jeong-sys <20211939@edu.hanbat.ac.kr>
2023-09-05 11:07:27 +0900

    keyword

--- src/main/java/project/plants/controller/ImageFinishController.java
+++ src/main/java/project/plants/controller/ImageFinishController.java
@@ -32,7 +32,7 @@ public class ImageFinishController {
         String condition_result = imageFinishService.send_DB_condition();
         System.out.println("condition_result : "+ condition_result);
         
-        String sql = "SELECT text FROM" + condition_result;
+        String sql = "SELECT text FROM " + condition_result + " ORDER BY id DESC LIMIT 2";
         
         try {
             jdbcTemplate.query(sql, new RowMapper<String>() {
@@ -42,19 +42,38 @@ public class ImageFinishController {
                     try {
                         String text = rs.getString("text");
                         System.out.println("text : " + text);
+                        textList.add(text);
                     } catch (Exception e) {
                         throw new SQLException("Failed to read the file.", e);
                     }
                     return null;
                 }
-            }).forEach(text -> textList.add(text));
+            });
 
         }catch(Exception e){
             System.out.println("SQL 문이 틀렸습니다");
             e.printStackTrace();
         }
         System.out.println("textList" + textList);
-        return textList;
+
+        ArrayList<String> rightList = new ArrayList<>();
+
+        String text_day1 = textList.get(1);
+        String text_day2 = textList.get(0);
+
+
+        // 여러개인 경우 어떻게 넣어야할지 생각하기
+        if (text_day1.contains("test1")) {
+            rightList.add("test1");
+        } else { rightList.add(""); }
+
+        if (text_day2.contains("test2")) {
+            rightList.add("test2");
+        } else { rightList.add(""); }
+
+        System.out.println("rightList" + rightList);
+
+        return rightList;
     }
 
     @GetMapping("/score_check")
